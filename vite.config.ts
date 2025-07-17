df2dc55fe410a0ec8f38c2e4962e0ecae97de8df
nohyewon <yw4913@naver.com>
2025-07-17 18:17:52 +0900

    :wrench:[chore]: 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+
+  // 절대경로 설정
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@constants', replacement: '/src/constants' },
+      { find: '@context', replacement: '/src/context' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@services', replacement: '/src/services' },
+      { find: '@utils', replacement: '/src/utils' },
+    ],
+  },
+});
