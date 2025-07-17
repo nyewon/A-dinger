2d436c137d1be40cdb135dd3e802fe7856f9b2ce
nohyewon <yw4913@naver.com>
2025-07-18 01:00:45 +0900

    :sparkles:[feat]: BottomNav 구현

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@
 export { default as DefaultHeader } from './Header/DefaultHeader';
 export { default as BackHeader } from './Header/BackHeader';
 export { default as SearchHeader } from './Header/SearchHeader';
+export { default as BottomNav } from './BottomNav';
