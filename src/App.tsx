33cfeafcfb53636687aa57b1064cfdfd26fc94d3
nohyewon <yw4913@naver.com>
2025-07-17 18:11:24 +0900

    :art:[style]: 화면 크기 및 비율 조정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <h1>Alzheimer-dinger_FE</h1>;
-}
+import styled from 'styled-components';
+
+const App = () => (
+  <Container>
+    <h1>Alzheimer-dinger_FE</h1>
+  </Container>
+);
 
 export default App;
+
+const Container = styled.div`
+  width: 100vw;
+  max-width: 425px;
+  min-width: 320px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+
+  // 텍스트 클릭 방지
+  user-select: none;
+  // 탭 하이라이트 제거
+  -webkit-tap-highlight-color: transparent;
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+
+  #root {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 );
