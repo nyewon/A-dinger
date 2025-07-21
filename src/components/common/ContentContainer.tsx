4700e4f9d904a5f79aa02fdc64b7bc949506fb9c
nohyewon <yw4913@naver.com>
2025-07-21 14:45:47 +0900

    :recycle:[refactor]: ContentContainer 변경

--- src/components/common/ContentContainer.tsx
+++ src/components/common/ContentContainer.tsx
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
   height: 100%;
+  align-items: center;
   gap: 1rem;
-  padding: 1.6rem 1.4rem;
+  padding: 2rem;
   margin-bottom: 70px;
   overflow-y: auto;
   box-sizing: border-box;
