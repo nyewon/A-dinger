0d091a152d3b96940261ca8dd32acdce56da5815
nyewon <yw4913@gmail.com>
2025-08-14 09:51:14 +0000

    :bug:[fix]: setupFetchInterceptor 주석처리

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { createGlobalStyle } from 'styled-components';
-import '@utils/setupFetchInterceptor';
+// import '@utils/setupFetchInterceptor';
 
 (async () => {
   try {
